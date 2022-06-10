00e082a4223ef0c32c16bba56cd5dba08943e495
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-06-10 15:45:20 +0530

    [Rajesh] Add : Added Flip Coin

---
+++ Programming/Programming/FlipCoin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming
+{
+    internal class FlipCoin
+    {
+        public void Coin()
+        {
+            int Heads = 0;
+            int Tails = 0;
+
+            Console.Write("Enter the Number of Flips to the Coin :");
+            int Flips= Convert.ToInt32(Console.ReadLine());
+
+            while (Flips <= 0)
+            {
+                Console.WriteLine("Invalid Input, Please Enter Number Greater than 0. ");
+                Console.Write("Please Enter Number of Flips Again :");
+                Flips = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < Flips; i++)
+            {
+                Random coin = new Random();
+                int flip = coin.Next(0, 2);
+
+                if (flip < 0.5)
+                {
+                    Heads++;
+                }
+                else
+                {
+                    Tails++;  
+                }
+
+            }
+
+            float Head_Percent = (float)Heads * 100 / Flips;
+            float Tail_Percent = (float)Tails * 100 / Flips;
+
+            Console.WriteLine("Percentage of Heads = " + Head_Percent + "%");
+            Console.WriteLine("Percentage of Tails = " + Tail_Percent + "%");
+        }
+    }
+}
--- Programming/Programming/Program.cs
+++ Programming/Programming/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Programming;
+
+Console.WriteLine("Enter a Number for Relative Problem.\n");
+Console.WriteLine("1 for Flip Coin and Print Percentage of Heads and Tails");
+
+
+int Problem = Convert.ToInt32(Console.ReadLine());
+
+switch (Problem)
+{
+    case 1:
+        FlipCoin flipCoin = new FlipCoin();
+        flipCoin.Coin();
+        break;
+}
