10ee40cc353725a3429f9ee7050a9f969b7925b7
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-06-10 16:04:40 +0530

    [Rajesh] Add : Added Power Of Two

--- Programming/Programming/Program.cs
+++ Programming/Programming/Program.cs
@@ -4,6 +4,7 @@ using Programming;
 Console.WriteLine("Enter a Number for Relative Problem.\n");
 Console.WriteLine("1 for Flip Coin and Print Percentage of Heads and Tails");
 Console.WriteLine("2 for Leep Year");
+Console.WriteLine("3 for Power of 2");
 
 
 int Problem = Convert.ToInt32(Console.ReadLine());
@@ -18,6 +19,10 @@ switch (Problem)
     case 2:
         LeapYear leapYear = new LeapYear();
         leapYear.Leap_Year();
+        break;
+    case 3:
+        PowerOfTwo powerOfTwo = new PowerOfTwo();
+        powerOfTwo.Power();
         break;
     default:
         Console.WriteLine("Invalid Option");
